6a6f333c635d2ec24b36e8bb2a9d7bf642e9e857
DESKTOP-52351T8\Admin <bf980326@gmail.com>
2024-11-15 15:52:47 +0900

    1. VR 튜토리얼 : grab 인터렉터 2. VR 튜토리얼 : UI 인터렉터 3. My Fps 메인 씬 1번 세팅

---
+++ Assets/Scripts/XRGrabInteractableOffset.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+namespace MyVrSample
+{
+    /// <summary>
+    /// 오브젝트를 잡을 때 오프셋 위치 설정
+    /// </summary>
+    public class XRGrabInteractableOffset : XRGrabInteractable
+    {
+        #region Variables
+        private GameObject attachPoint;
+
+        private Vector3 initialLocalPosition;
+        private Quaternion initialLocalRotation;
+        #endregion
+
+        private void Start()
+        {
+            if(attachTransform == null)
+            {
+                attachPoint = new GameObject("Offset Grab Pivot");
+                attachPoint.transform.SetParent(transform, false);
+                attachTransform = attachPoint.transform;
+            }
+            else
+            {
+                initialLocalPosition = attachTransform.localPosition;
+                initialLocalRotation = attachTransform.localRotation;
+            }
+        }
+
+        //protected override void OnSelectEntering(SelectEnterEventArgs args)
+        //{
+        //    attachTransform.position = args.interactorObject.transform.position;
+        //    attachTransform.rotation = args.interactorObject.transform.rotation;
+
+        //    base.OnSelectEntered(args);
+        //}
+
+
+        protected override void OnSelectEntered(SelectEnterEventArgs args)
+        {
+            if(args.interactorObject is XRDirectInteractor)
+            {
+                attachTransform.position = args.interactorObject.transform.position;
+                attachTransform.rotation = args.interactorObject.transform.rotation;
+            }
+            else
+            {
+                attachTransform.localPosition = initialLocalPosition;
+                attachTransform.localRotation = initialLocalRotation;
+            }
+            
+
+            base.OnSelectEntered(args);
+        }
+
+    }
+}
+
--- Assets/Scripts/XRGrabInteractableTwoAttach.cs
+++ Assets/Scripts/XRGrabInteractableTwoAttach.cs
@@ -14,7 +14,7 @@ namespace MyVrSample
         public Transform rightAttachTransform;
         #endregion
 
-        protected override void OnSelectEntered(SelectEnterEventArgs args)
+        protected override void OnSelectEntering(SelectEnterEventArgs args)
         {
             // 두개의 Attach Point 를 잡는 손에 따라 구분해서 적용
             if (args.interactorObject.transform.CompareTag("LeftHand"))
@@ -28,6 +28,21 @@ namespace MyVrSample
 
             base.OnSelectEntered(args);
         }
+
+        //protected override void OnSelectEntered(SelectEnterEventArgs args)
+        //{
+        //    // 두개의 Attach Point 를 잡는 손에 따라 구분해서 적용
+        //    if (args.interactorObject.transform.CompareTag("LeftHand"))
+        //    {
+        //        attachTransform = leftAttachTransform;
+        //    }
+        //    else if (args.interactorObject.transform.CompareTag("RightHand"))
+        //    {
+        //        attachTransform = rightAttachTransform;
+        //    }
+
+        //    base.OnSelectEntered(args);
+        //}
     }
 }
 
